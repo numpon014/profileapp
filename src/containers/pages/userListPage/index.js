7463fbfd143d680515a0d0c1fcc257c5b05d055d
numpon014 <numpon014@gmail.com>
2022-01-09 12:12:48 +0700

    create buttons

--- src/containers/pages/userListPage/index.js
+++ src/containers/pages/userListPage/index.js
@@ -4,8 +4,9 @@ import styled from "styled-components";
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { Card, Col, Row } from "react-bootstrap";
-import { BsEnvelope, BsTelephone, BsGlobe } from "react-icons/bs";
+import {Button, Card, Col, Row, Container} from "react-bootstrap";
+import { BsEnvelope, BsTelephone, BsGlobe, BsHeart, BsTrash } from "react-icons/bs";
+import { FiEdit3 } from "react-icons/fi";
 import { getUsers } from '../../../shares/actions/user';
 
 const StyledWrap = styled.div`
@@ -13,12 +14,23 @@ const StyledWrap = styled.div`
     height: 150px;
     background: #f5f6fa;
   }
-  
+
   .icon {
     padding-right: 10px;
-    
+
     svg {
       vertical-align: -0.125em;
+    }
+  }
+
+  .profile-buttons {
+    .button-cols {
+      text-align: center;
+      border-left: #d5d7e0 1px solid;
+
+      &:first-child {
+        border-left: 0 none;
+      }
     }
   }
 `;
@@ -37,39 +49,56 @@ function UserListPage({ users, getAllUsers }) {
           content='user list page'
         />
       </Helmet>
-      <Row xs={1} md={4} className="g-4">
-        { users.map(user => (
-          <Col key={`${user.id}-${user.username}`}>
-            <Card>
-              <Card.Img variant="top"
-                        className="profile-image"
-                        src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
-              />
-              <Card.Body>
-                <Card.Title>{user.name}</Card.Title>
-                <address>
-                  <div className="email">
-                    <span className="icon"><BsEnvelope/></span>
-                    <span>{user.email}</span>
-                  </div>
-                  <div className="phone">
-                    <span className="icon"><BsTelephone/></span>
-                    <span>{user.phone}</span>
-                  </div>
-                  <div className="website">
-                    <span className="icon"><BsGlobe/></span>
-                    <span>{user.website}</span>
-                  </div>
-                </address>
-              </Card.Body>
-              <Card.Footer>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Footer>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <Container>
+        <Row xs={1} md={4} className="g-4">
+          { users.map(user => (
+            <Col key={`${user.id}-${user.username}`}>
+              <Card>
+                <Card.Img variant="top"
+                          className="profile-image"
+                          src={`https://avatars.dicebear.com/v2/avataaars/${user.username.toLowerCase()}.svg?options[mood][]=happy`}
+                />
+                <Card.Body>
+                  <Card.Title>{user.name}</Card.Title>
+                  <address>
+                    <div className="email">
+                      <span className="icon"><BsEnvelope/></span>
+                      <span>{user.email}</span>
+                    </div>
+                    <div className="phone">
+                      <span className="icon"><BsTelephone/></span>
+                      <span>{user.phone}</span>
+                    </div>
+                    <div className="website">
+                      <span className="icon"><BsGlobe/></span>
+                      <span>{user.website}</span>
+                    </div>
+                  </address>
+                </Card.Body>
+                <Card.Footer className="profile-buttons">
+                  <Row>
+                    <Col sm className="button-cols">
+                      <Button className="like-btn" variant="link">
+                        <BsHeart size={18} color="red"/>
+                      </Button>
+                    </Col>
+                    <Col sm className="button-cols">
+                      <Button className="edit-btn" variant="link">
+                        <FiEdit3 size={18} color="gray"/>
+                      </Button>
+                    </Col>
+                    <Col sm className="button-cols">
+                      <Button className="delete-btn" variant="link">
+                        <BsTrash size={18} color="gray"/>
+                      </Button>
+                    </Col>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </StyledWrap>
   );
 }
